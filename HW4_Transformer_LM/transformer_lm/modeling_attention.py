b2e4c4d912a4838613aa1ec6a06d8d80f5debe8c
Jean Disset <j.disset@gmail.com>
2022-02-23 00:41:52 -0500

    cleaned up code

--- HW4_Transformer_LM/transformer_lm/modeling_attention.py
+++ HW4_Transformer_LM/transformer_lm/modeling_attention.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-# Copyright 2022 Vladislav Lialin and Namrata Shivagunde 
+# Copyright 2022 Vladislav Lialin and Namrata Shivagunde
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ class SelfAttention(nn.Module):
         Returns:
             FloatTensor[batch_size, seq_len, hidden]
         """
-        
+
         # Task 1.1: Compute Self Attention (3 points)
         # 1. Compute key, query and value matrices from your input x using self.k, self.q and self.v
         # 2. Compute the scores using query and key matrices
@@ -52,26 +52,23 @@ class SelfAttention(nn.Module):
         # Please write shape of each tensor for each line of code
         # for example:
         #       Suppose batch_size = 3 and seq_len = 5
-        #       x = torch.zeros(3, 5) # shape [batch_size, seq_len] 
+        #       x = torch.zeros(3, 5) # shape [batch_size, seq_len]
         #       x = x.unqueeze(1)     # shape [batch_size, 1, seq_len]
-        # 
+        #
         # NOTE: Remmenber that we work with batches of data [batch_size, seq_len, hidden],
         # not just single examples [seq_len, hidden] as we did in the lecture. This changes your shapes a bit.
         #
         # YOUR CODE STARTS HERE (~ can be implemented in 4 lines or 3 if you combine steps 2 and 3 into one operation)
-        #https://stackoverflow.com/questions/61764582/how-does-the-transformer-model-compute-self-attention
- 
-        q, k, v = self.q(x), self.k(x), self.v(x)
-        print(x.shape, q.shape)
+        # https://stackoverflow.com/questions/61764582/how-does-the-transformer-model-compute-self-attention
+
+        q, k, v = self.q(x), self.k(x), self.v(x) 
+        # q -> [batch_size, q_len, hidden]
+        # k -> [batch_size, k_len, hidden]
+        # v -> [batch_size, v_len, hidden]
         qkt = (torch.matmul(q, k.transpose(-2, -1))) / self.scale
-        print(q, k, v)
+        # qkt -> [batch_size, q_len, k_len]
         attention = torch.bmm(F.softmax(qkt, dim=2), v)
-
-
-
-
-
-
+        # attention -> [batch_size, q_len, hidden]
 
         # YOUR CODE ENDS HERE
 
@@ -118,15 +115,15 @@ class MultiHeadSelfAttention(nn.Module):
         Returns:
             FloatTensor[batch_size, seq_len, hidden]
         """
-       
+
         bs, seq, _ = x.shape
- 
+
         # Task 1.2  (1 point)
         # 1. Compute key, query and value matrices from your input x using self.k, self.q and self.v
         # 1. Split them into multiple heads for multihead attention.
         # This can be achieves as a sequence of transpose and reshape operations.
         # Hint: Your target shape is [batch * num_heads, seq, hidden / num_heads]
-        # 
+        #
         # NOTE: notice that reshape and transpose operations are different,
         # for example, given a tensor of shape [M, N] .reshape (N, M) and .transpose (1, 0)
         # will return you **different** tensors even thought their shapes are the same.
@@ -135,15 +132,21 @@ class MultiHeadSelfAttention(nn.Module):
         # Please write how the shape of the tensors are changing after each operation
         # for example:
         #        suppose 'a' is a tensor of shape [batch_size, input_size] and we apply following operation on it
-        #        a = a.unsqueeze(1).transpose() # shape [batch_size, input_size, 1] 
+        #        a = a.unsqueeze(1).transpose() # shape [batch_size, input_size, 1]
         #        # 'a' [batch_size, input_size] -> unsqueeze [batch_size, 1, input_size] -> transpose [batch_size, input_size, 1]
         #
         # This comment explains how each operation is changing the shape of the tensor 'a' before it finally gets assigned to 'a' again.
         # This is just an example and does not suggest if these operations are used for this task.
         # YOUR CODE STARTS HERE (Our implementation is in 3 lines, one for each for k, q and v)
-        q = self.q(x).view(x.size(0), -1, self.num_heads, self.head_size).permute(0,2,1,3)
-        k = self.k(x).view(x.size(0), -1, self.num_heads, self.head_size).permute(0,2,1,3)
-        v = self.v(x).view(x.size(0), -1, self.num_heads, self.head_size).permute(0,2,1,3)
+        q = self.q(x).view(bs, -1, self.num_heads,
+                           self.head_size).permute(0, 2, 1, 3)
+        # q -> [batch_size, num_heads, q_len, head_size]
+        k = self.k(x).view(x.size(0), -1, self.num_heads,
+                           self.head_size).permute(0, 2, 1, 3)
+        # k -> [batch_size, num_heads, k_len, head_size]
+        v = self.v(x).view(x.size(0), -1, self.num_heads,
+                           self.head_size).permute(0, 2, 1, 3)
+        # v -> [batch_size, num_heads, v_len, head_size]
 
         # YOUR CODE ENDS HERE
 
@@ -151,9 +154,9 @@ class MultiHeadSelfAttention(nn.Module):
         # 1. Compute scores (query key product) and scale them
         # YOUR CODE STARTS HERE  (our implementation is in 1 line)
 
+        scores = torch.matmul(q, k.transpose(-2, -1)) / self.scale
+        # scores -> [batch_size, num_heads, q_len, k_len]
 
-        scores  = torch.matmul(q, k.transpose(-2, -1)) / self.scale
-       
         # your code ends here
 
         if self.causal:
@@ -162,18 +165,22 @@ class MultiHeadSelfAttention(nn.Module):
             # 1. Create a casual_mask that does not allow queries to look to the future keys
             # Specify the device of the causal_mask tensor to be the same as the scores.device
             # 2. Apply this casual mask on scores, fill with '-inf' where the mask is present.
-            # 
+            #
             # You will find the following functions useful:
             #  - torch.triu
             #  - .masked_fill_
             #
             # NOTE : Please write shape of the tensor for each line of code
             # YOUR CODE STARTS HERE (Our implementation is in 2 lines)
-            causal_mask = torch.tril(torch.ones((x.size(1), x.size(1)))).expand(x.size(0), 1, x.size(1), x.size(1)).to(scores.device)
-            scores  = scores.masked_fill(causal_mask==0, -float('inf'))
+            causal_mask = torch.tril(
+                    torch.ones((x.size(1), x.size(1)))).expand(
+                    x.size(0), 1, x.size(1), x.size(1)
+                    ).to(scores.device)
+            # causal_mask -> [batch_size, 1, seq, seq]
+            scores = scores.masked_fill(causal_mask == 0, -float('inf'))
+            # scores ->
+            # scores -> [batch_size, num_heads, q_len, k_len]
 
-
-            # def look_ahead_mask(tgt_len:int, src_len:int) -> torch.FloatTensor:
     # """ this will be applied before sigmoid function, so '-inf' for proper positions needed.
     # look-ahead masking is used for decoder in transformer,
     # which prevents future target label affecting past-step target labels. """
@@ -191,7 +198,7 @@ class MultiHeadSelfAttention(nn.Module):
         # 4. Apply a number of matrix transformation on attention to change its dimenion to [batch, seq, hidden] (Our implmentation has four operations)
         # 5. Mix the attentions using self.mix, name the output `att`
         # Please write shape of the tensor for each line of code
-        # 
+        #
         # NOTE: correct shapes do not guarantee correctness of the code.
         # You should understand how the reshapes and transposes are changing the elements of the tensor.
         #
@@ -199,10 +206,15 @@ class MultiHeadSelfAttention(nn.Module):
         # import ipdb
         # ipdb.set_trace()
         probs = F.softmax(scores, dim=-1)
+        # probs -> [batch_size, num_heads, q_len, k_len]
         probs = self.dropout(probs)
-        att = torch.matmul(probs, v).permute(0,2,1,3)
+        # probs -> [batch_size, num_heads, q_len, k_len]
+        att = torch.matmul(probs, v).permute(0, 2, 1, 3)
+        # att -> [batch_size, q_len, num_heads, head_size]
         probs = probs.reshape(bs * self.num_heads, seq, -1)
+        # probs -> [batch_size * num_heads, seq, seq]
         att = self.mix(att.reshape(*att.shape[:2], -1))
+        # att -> [batch_size, seq, hidden]
 
         # YOUR CODE ENDS HERE
 
